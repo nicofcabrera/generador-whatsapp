ad40b808c9e8313d7c4a061c55dff6a516482a84
unknown <nico.fcok@gmail.com>
2022-12-24 17:23:18 -0300

    subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const wsp = 'https://wa.me/54'
+
+const gererarChat = () => {
+  console.log('funca chat')
+  const numero = document.getElementById('numero').value;
+  const mensaje = document.querySelector('.mensaje');
+
+
+   if (numero) {
+    window.open(wsp+numero,'_blank')
+  } else {
+    mensaje.innerHTML = `
+    <div class="alert alert-danger" role="alert">
+      El numero debe tener 10 digitos
+    </div> `
+  }
+ 
+
+}
+
+const gererarLink = () => {
+  console.log('funca link')
+  const numero = document.getElementById('numero').value;
+  const mensaje = document.querySelector('.mensaje');
+  
+  if (numero.length == 10) {
+    mensaje.innerHTML = `
+    <div class="alert alert-success" role="alert">
+      El link es <b>${wsp}${numero}</b>
+    </div> `
+  } else {
+    mensaje.innerHTML = `
+    <div class="alert alert-danger" role="alert">
+      El numero debe tener 10 digitos
+    </div> `
+  }
+  
+      
+}
+
